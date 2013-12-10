fb6bb3111d7677bb9af97fd418ed696750622f5e
mrgreaper <mrgreaper2004@gmail.com>
2013-12-10 06:49:43 +0000

    added range check

--- TwistedMod_common/com/mrgreaper/twisted/tileentities/TileEntitySlavedBunny.java
+++ TwistedMod_common/com/mrgreaper/twisted/tileentities/TileEntitySlavedBunny.java
@@ -1,5 +1,8 @@
 package com.mrgreaper.twisted.tileentities;
 
+import java.util.Iterator;
+import java.util.List;
+
 import com.mrgreaper.twisted.client.sounds.SpeechHandler;
 
 import dan200.computer.api.IComputerAccess;
@@ -11,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
 public class TileEntitySlavedBunny extends TileEntity implements IPeripheral{
@@ -38,9 +42,36 @@ public class TileEntitySlavedBunny extends TileEntity implements IPeripheral{
 			float vPitchRange = ((Number) arguments[1]).floatValue();
 			float vPitchShift = ((Number) arguments[2]).floatValue();
 			String vSentence = (arguments[3].toString());
+			//bellow here is where i try to get players nearby
+			double vRange = 15;
+			//get bounding box (using beacon code.....this could end badly)
+			AxisAlignedBB axisalignedbb =
+					AxisAlignedBB.getAABBPool().getAABB((double)this.xCoord, (double)this.yCoord, (double)this.zCoord, (double)(this.xCoord + 1), (double)(this.yCoord + 1), (double)(this.zCoord + 1)).expand(vRange, vRange, vRange);
+			//don't check above the map
+            axisalignedbb.maxY = (double)this.worldObj.getHeight();
+          //get all players in the bounding box
+            List list = this.worldObj.getEntitiesWithinAABB(EntityPlayer.class, axisalignedbb);
+          //iterate
+            Iterator iterator = list.iterator();
+            EntityPlayer entityplayer;
+//iterate
+            while (iterator.hasNext())
+            {
+                    entityplayer = (EntityPlayer)iterator.next(); //would be nice if i could get the range the player is from the tileentity to control the volume....some day 
 			//SpeechHandler.speechSynth(world, entityname, vVoice, vPitch, vPitchRange, vPitchShift, vSentence);
 			SpeechHandler.speechSynth(null, null, vVoice, vPitch, vPitchRange, vPitchShift, vSentence);
+			
+			 {
+                 iterator = list.iterator();
+//iterate again
+                 while (iterator.hasNext())
+                 {
+                         entityplayer = (EntityPlayer)iterator.next();
 		}
+			 }
+            }
+		}
+		
 		default:
 		return null;
 		}
--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
+set ANT_HOME=c:\dev\ant
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_25
+set PATH=%PATH%;%ANT_HOME%\bin
 ant
-pause
+pause 
